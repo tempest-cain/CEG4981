aaccd63e06db75fc4edcd048c8bebf6d75e5477f
John Klaassen <jogaklaa@gmail.com>
2018-02-13 10:08:33 -0500

    The start of my modeling career!

--- README.md
+++ README.md
@@ -1,11 +1,1 @@
 CEG4981
-
-
-         88                                                
-         88                                                
-         88                                                
- ,adPPYb,88 8b,dPPYba,  ,adPPYba,  8b,dPPYba,   ,adPPYba,  
-a8"    `Y88 88P'   "Y8 a8"     "8a 88P'   `"8a a8P_____88  
-8b       88 88         8b       d8 88       88 8PP"""""""  
-"8a,   ,d88 88         "8a,   ,a8" 88       88 "8b,   ,aa  
- `"8bbdP"Y8 88          `"YbbdP"'  88       88  `"Ybbd8"'  
--- webServer/parkingService/carCheck/models.py
+++ webServer/parkingService/carCheck/models.py
@@ -3,4 +3,20 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class parking_pass(models.Model):
+    pass_number = models.IntegerField( unique=True)
+    expiration = models.CharField(max_length=30)
+    owner = models.CharField(max_length=300) #would be student ID in real applicatoin
+
+class car(models.Model):
+    model = models.CharField(max_length = 100)
+    brand = models.CharField(max_length = 100)
+    licence_plate = models.CharField(max_length=12)
+    color = models.CharField(max_length = 30)
+    parking_pass = models.ForeignKey(parking_pass)
+
+class ticket(models.Model):
+    car = models.ForeignKey(car)
+    parking_pass = models.ForeignKey(parking_pass, blank=True)
+    fine_amount = models.DecimalField(decimal_places=2, max_digits=10)
+    photo = models.FileField()
